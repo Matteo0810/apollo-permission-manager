86883e64ba413d9e6c64960756416dbc1f71544f
matteogaillard <matteo.gaillard@protonmail.com>
2024-05-19 14:12:40 +0200

    clearning folders

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const PermissionManager = require("./lib/PermissionManger.js");
-
-function usePermissionManger(options) {
-    const permissionManger = new PermissionManager(options);
-    return (req, res, next) => {
-        const token = req.headers.authorization || "";
-        next();
-    }
-}
-
-module.exports = { usePermissionManger };
+module.exports = require("./lib");
